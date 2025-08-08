7ad79c657f042d79c6e6e6d458127b849ead6746
PangyaJ <starimhj@gmail.com>
2025-08-08 18:29:30 +0900

    Update mobileNet_v3 - Training (General & Transfer Learning).py

--- mobileNet_v3 - Training (General & Transfer Learning).py
+++ mobileNet_v3 - Training (General & Transfer Learning).py
@@ -154,7 +154,7 @@ def main():
     
     #=============================================================================#
     # 학습된 모델 가중치 로드
-    pretrained_path = ('C:/Users/Evan/OneDrive/바탕 화면/VScode/[1st]train/[mobileNet_v3s]trained_mobilenetv3s_model_best_epoch_24_Loss_0.3817.pth')
+    pretrained_path = ('C:/Users/Evan/OneDrive/바탕 화면/VScode/[1st]train/[mobileNet_v3s]trained_mobilenetv3s_model_best_epoch_37_Loss_0.0317.pth')
     if os.path.exists(pretrained_path):
         model.load_state_dict(torch.load(pretrained_path, map_location=device))
         print(f"사전 학습된 모델이 {pretrained_path}에서 성공적으로 로드되었습니다.")
